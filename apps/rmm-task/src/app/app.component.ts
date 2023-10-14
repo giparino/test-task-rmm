b388483df4edc367d8f6282f56addc4a680f12ba
Timur Mishagin <timur@mishagin.com>
2023-10-14 14:42:21 +0300

    Create menu lib

--- apps/rmm-task/src/app/app.component.ts
+++ apps/rmm-task/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MenuComponent } from '@rmm-task/menu';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [MenuComponent, RouterModule],
   selector: 'rmm-task-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
