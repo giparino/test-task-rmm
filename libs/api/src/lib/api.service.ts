9665aba0b8b729cd775a51bc0fe49f3a42ec951b
Timur Mishagin <timur@mishagin.com>
2023-10-15 20:57:13 +0300

    Prepare files for task

---
+++ libs/api/src/lib/api.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { animals, uniqueNamesGenerator } from 'unique-names-generator';
+import { v4 as uuidv4 } from 'uuid';
+import { icons, ItemInterface } from './api.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private apiPath = 'http://localhost:3000/animals';
+
+  constructor(private http: HttpClient) {}
+
+  public getAllItems$(): Observable<ItemInterface[]> {
+    return this.http.get<ItemInterface[]>(this.apiPath);
+  }
+
+  public addNewItem$(item: ItemInterface): Observable<ItemInterface> {
+    return this.http.post<ItemInterface>(this.apiPath, item);
+  }
+
+  public deleteItem$(item: ItemInterface): Observable<ItemInterface> {
+    return this.http.delete<ItemInterface>(`${this.apiPath}/${item.id}`);
+  }
+}
+
+export const generateRandomItem = (): ItemInterface => {
+  return {
+    id: uuidv4(),
+    name: uniqueNamesGenerator({ dictionaries: [animals] }),
+    icon: icons[Math.floor(Math.random() * icons.length)] || 'android',
+  };
+};
--- libs/menu/src/lib/menu/menu.component.ts
+++ libs/menu/src/lib/menu/menu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'libs-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
